aa75f4cca391c16f3f8d79a886a44c477780d636
Aline-sts <105322506+Aline-sts@users.noreply.github.com>
2022-08-19 17:48:45 -0300

    Add files via upload

---
+++ Japan.java
@@ -0,0 +1,23 @@
+
+public class Japan {
+    public static void main(String[] args) {
+        String palavra= "arvore";
+
+        switch (palavra){
+            case "arvore":
+                System.out.println("É arvore 木");
+                break;
+            case "casa":
+                System.out.println("casa ホ");
+                break;
+            case "gato":
+                System.out.println("猫");
+                break;
+            case "amor":
+                System.out.println("愛");
+                break;
+            default:
+                System.out.println("Palavra invalida");
+        }
+    }
+}
