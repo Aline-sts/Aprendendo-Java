e4c3d46a97d6719c30d69cd2f45d8fa5e6d5fd98
Aline-sts <105322506+Aline-sts@users.noreply.github.com>
2022-08-25 11:30:27 -0300

    Update Japan.java

--- Japan.java
+++ Japan.java
@@ -1,20 +1,33 @@
 
 public class Japan {
     public static void main(String[] args) {
+        //Aqui fiz uma inicialização já com o valor da String
         String palavra= "arvore";
 
         switch (palavra){
             case "arvore":
-                System.out.println("É arvore 木");
+                System.out.println(" 木");
                 break;
             case "casa":
-                System.out.println("casa ホ");
+                System.out.println(" ホ");
                 break;
             case "gato":
                 System.out.println("猫");
                 break;
             case "amor":
                 System.out.println("愛");
+                break;
+                 case "paz":
+                System.out.println("平和");
+                break;
+                 case "uniao":
+                System.out.println("団結");
+                break;
+                 case "familia":
+                System.out.println("家族");
+                break;
+                 case "Japao":
+                System.out.println("日本");
                 break;
             default:
                 System.out.println("Palavra invalida");
